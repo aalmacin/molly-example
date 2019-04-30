c61dd5143497648e13e17151737bc3fe75309b7f
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2019-04-30 18:21:01 -0400

    Usage of molly component library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MollyComponentLibraryModule } from 'molly-component-library';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    MollyComponentLibraryModule,
     BrowserModule,
     AppRoutingModule
   ],
